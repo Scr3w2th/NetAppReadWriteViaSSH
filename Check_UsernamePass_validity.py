1f130579788f2d8347c6f6fadf87640241cf0c9a
David Lee <dlee74@msudenver.edu>
2024-01-13 20:23:17 -0700

    Updated Check_User/Pass mod to include the SSH connect, via paramiko. Also added: cmd.txt, user.txt, and requirements.txt files.

--- Check_UsernamePass_validity.py
+++ Check_UsernamePass_validity.py
@@ -28,3 +28,85 @@ else:
     print("\n* File {} does not exist :( Please check and try again.\n".format(cmd_file))
     sys.exit()
     
+# Open SSH connection to the device
+def ssh_connection(ip):
+    
+    global user_file
+    global cmd_file
+    
+    # Create SSH CONNECTION
+    try:
+        # Define SSH Params
+        selected_user_file = open(user_file, 'r')
+        
+        # Starting from the beginning of the file
+        selected_user_file.seek(0)
+        
+        # Reading the username from the file
+        username = selected_user_file.readlines()[0].split(',')[0].rstrip("\n")
+        
+        # Starting from the beginning of the file
+        selected_user_file.seek(0)
+        
+        # Reading the password from the file
+        password = selected_user_file.readlines()[0].split(',')[1].rstrip("\n")
+        
+        # Logging into device
+        session = paramiko.SSHClient()
+        
+        # For testing purposes this allows auto-accepting unknown host keys, DO NOT USE IN PROD!!! Default is 'RejectPolicy'
+        session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        
+        # Connect to the device using username and password
+        session.connect(ip.rstrip("\n"), username = username, password = password)
+        
+        # Start an interactive shell session on the router
+        connection = session.invoke_shell()
+        
+        # Setting terminal length for entire output - disable pagination
+        connection.send("enable\n")
+        connection.send("terminal length 0\n")
+        time.sleep(1)
+        
+        # Entering global config mode
+        connection.send("\n")
+        connection.send("configure terminal\n")
+        time.sleep(1)
+        
+        # Open user selected file for reading
+        selected_cmd_file = open(cmd_file, 'r')
+        
+        # Starting from the beginning of the file
+        selected_cmd_file.seek(0)
+        
+        # Writting each line in the file to the device
+        for each_line in selected_cmd_file.readlines():
+            connection.send(each_line + '\n')
+            time.sleep(2)
+            
+        # Closing the user file
+        selected_user_file.close()
+        
+        # Closing the command file
+        selected_cmd_file.close()
+        
+        # Checking command output for IOS syntax errors
+        router_output = connection.recv(65535)
+        
+        if re.search(b"% Invalid input", router_output):
+            print("* There was at least one IOS syntax error on device {} :(".format(ip))
+            
+        else:
+            print("\nDONE for device () :)\n".format(ip))
+            
+        # Test for reading command output
+        print(str(router_output) + "\n")
+        
+        # Closing the connection
+        session.close()
+        
+    except paramiko.AuthenticationException:
+        print("* Invalid username or password :( \n* Please check the username/password file or the device configuration.")
+        print("* Closing program... Bye!")
+        
+        
